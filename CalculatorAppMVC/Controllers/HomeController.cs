3c1d490773604e15ab9a2a5d97accb79dab32a01
BeyzaKayraklik <76918032+BeyzaKayraklik@users.noreply.github.com>
2021-11-02 20:26:25 +0300

    internship project2

---
+++ CalculatorAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using CalculatorAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Calculator cal)
+        {
+            int a, b;
+            a = cal.value1;
+            b = cal.value2;
+            if (cal.calculate == "Topla")
+            {
+                cal.result = a + b;
+            }
+            else if (cal.calculate == "Çıkar")
+            {
+                cal.result = a - b;
+            }
+            else if (cal.calculate == "Çarp")
+            {
+                cal.result = a * b;
+            }
+            else if (cal.calculate == "Böl")
+            {
+                cal.result = a / b;
+            }
+
+
+            TempData["result"] = cal.result;
+
+            return View();
+        }
+
+        public IActionResult SonucSayfası()
+        {
+            var snc = TempData["result"];
+
+            TempData.Keep();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CalculatorAppMVC/Models/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorAppMVC.Models
+{
+    public class Calculator
+    {
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+        public int result { get; set; }
+        public string calculate { get; set; }
+    }
+}
